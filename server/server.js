e14cc0a900c051dd720723e923fe2965410b9761
varshaps27 <psvarsha27082005@gmail.com>
2025-06-24 16:19:31 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -81,53 +81,5 @@ const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
-/*require("dotenv").config();
-const express = require("express");
-const http = require("http");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const { Server } = require("socket.io");
-const authRoutes = require("./routes/auth");
 
 
-mongoose.connect(process.env.MONGO_URI || "mongodb+srv://psvarsha27082005:pXFmFpp1fh40D0gb@cluster0.yck3upb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(() => console.log("✅ MongoDB Connected"))
-  .catch(console.error);
-
-const app = express();
-app.use(cors());
-app.use(express.json());
-app.use("/api/auth", authRoutes);
-
-const server = http.createServer(app);
-const io = new Server(server, {
-  cors: { origin: "*", methods: ["GET", "POST"] }
-});
-
-io.on("connection", (socket) => {
-  console.log(`🟢 User connected: ${socket.id}`);
-
-  socket.on("joinRoom", ({ roomId, username }) => {
-    socket.join(roomId);
-    io.to(roomId).emit("userJoined", `${username} joined "${roomId}"`);
-    console.log(`${username} → ${roomId}`);
-  });
-
-  socket.on("sendMessage", ({ roomId, username, message }) => {
-    console.log(`📨 Message from ${username} in ${roomId}: ${message}`);
-    io.to(roomId).emit("receiveMessage", {
-      username,
-      message,
-      timestamp: new Date(),
-    });
-});
-
-  socket.on("typing", ({ roomId, username }) => {
-    socket.broadcast.to(roomId).emit("typing", username);
-  });
-
-  socket.on("disconnect", () => console.log(`🔴 Disconnected: ${socket.id}`));
-});
-
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
-*/
